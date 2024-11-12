504b37ae1b89220f1d7ea3686c9f1c1ce8f022f0
yuu0305 <segyayuu8@gmail.com>
2024-11-13 08:49:23 +0900

    1-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # get 'pages/about'
+  get 'about', to: 'pages#about'
   resources :contacts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
