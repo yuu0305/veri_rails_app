e72cd759dde26dfc979cfce0b7e8307de4a2d399
yuu0305 <segyayuu8@gmail.com>
2024-11-13 09:40:51 +0900

    1-2　修正

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def about
   end
+
+  def home
+  end
+
+  def contact
+  end
+
+  def services
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  # get 'pages/about'
+  get 'home', to: 'pages#home'
+  get 'services', to: 'pages#services'
   get 'about', to: 'pages#about'
   resources :contacts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
